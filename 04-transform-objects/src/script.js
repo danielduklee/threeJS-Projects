d52186eeb0f2c5cf972d39b3ca8bdfcdcc437834
Daniel Lee <iphone.work.kt@gmail.com>
2025-09-03 14:07:41 -0700

    04-Group

--- 04-transform-objects/src/script.js
+++ 04-transform-objects/src/script.js
@@ -9,27 +9,32 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const group = new THREE.Group();
+group.position.y = 1
+group.scale.y = 2
+group.rotation.y = 1
+scene.add(group)
 
-// Position
-// mesh.position.x = 0.7;
-// mesh.position.y = -0.6;
-// mesh.position.z = 1;
-mesh.position.set(0.7, -0.6, 1);
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
 
-// Scale
-// mesh.scale.x = 2
-// mesh.scale.y = 0.5
-// mesh.scale.z = 0.5
-mesh.scale.set(2, 0.5, 0.5);
+group.add(cube1);
 
-// Rotation
-mesh.rotation.reorder('YXZ');
-mesh.rotation.x = Math.PI * 0.25
-mesh.rotation.y = Math.PI * 0.25
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+)
+cube2.position.x = -2;
+group.add(cube2);
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x0000ff })
+)
+cube3.position.x = 2;
+group.add(cube3);
 
 // Axes helper
 const axesHelper = new THREE.AxesHelper()
@@ -53,7 +58,6 @@ camera.position.z = 3
 // camera.position.x = 1
 scene.add(camera)
 
-camera.lookAt(mesh.position)
 
 /**
  * Renderer
