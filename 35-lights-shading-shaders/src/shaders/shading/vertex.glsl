2068c9e591f2bce9c18c442b16a7311e0925a8a9
Daniel Lee <iphone.work.kt@gmail.com>
2025-10-13 11:57:36 -0700

    37-lights shading shaders

---
+++ 35-lights-shading-shaders/src/shaders/shading/vertex.glsl
@@ -0,0 +1,15 @@
+varying vec3 vNormal;
+varying vec3 vPosition;
+
+void main()
+{
+    vec4 modelNormal = modelMatrix * vec4(normal, 0.0);
+
+    // Position
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    gl_Position = projectionMatrix * viewMatrix * modelPosition;
+
+    // Varying
+    vNormal = modelNormal.xyz;
+    vPosition = modelPosition.xyz;
+}
